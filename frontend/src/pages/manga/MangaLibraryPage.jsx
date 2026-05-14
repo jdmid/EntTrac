a00190255fafffe32ce1769e47cb4f87c50c11e8
Joseph <josephmiddlebrook@gmail.com>
2026-05-14 14:53:05 -0500

    feat: scaffold MangaLibraryPage and MangaSearchPage.

--- frontend/src/pages/manga/MangaLibraryPage.jsx
+++ frontend/src/pages/manga/MangaLibraryPage.jsx
@@ -1,5 +1,14 @@
+import Navbar from '../../components/Navbar'
+
 function MangaLibraryPage() {
-  return <div>Manga Library</div>
+  return (
+    <div>
+      <Navbar activeMedia="manga" />
+      <div className="p-5">
+        <p className="text-white">Manga Library</p>
+      </div>
+    </div>
+  )
 }
 
 export default MangaLibraryPage
--- frontend/src/pages/manga/MangaSearchPage.jsx
+++ frontend/src/pages/manga/MangaSearchPage.jsx
@@ -1,5 +1,14 @@
+import Navbar from '../../components/Navbar'
+
 function MangaSearchPage() {
-  return <div>Manga Search</div>
+  return (
+    <div>
+      <Navbar activeMedia="manga" />
+      <div className="p-5">
+        <p className="text-white">Manga Search</p>
+      </div>
+    </div>
+  )
 }
 
 export default MangaSearchPage
